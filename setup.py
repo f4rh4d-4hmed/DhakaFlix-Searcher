2b2bcf0340e4a1997b1ad4df36d8d619fcdcbc0b
Farhad Ahmed <161406872+f4rh4d-4hmed@users.noreply.github.com>
2025-10-27 11:52:48 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from cx_Freeze import setup, Executable
+import sys
+import os
+import PyQt6
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+qt_plugins_dir = os.path.join(os.path.dirname(PyQt6.__file__), "Qt6", "plugins")
 
 build_exe_options = {
-    "packages": ["requests", "bs4", "pyqt6"],
-    "include_files": ["Jaro-Regular.ttf"],
+    "packages": ["requests", "bs4", "PyQt6"],
+    "include_files": [
+        "Jaro-Regular.ttf",
+        (qt_plugins_dir, "plugins"),
+    ],
+    "excludes": ["tkinter"],
 }
 
 setup(
@@ -10,5 +23,5 @@ setup(
     version="0.1.1",
     description="DFLIX-Test v0.1.1",
     options={"build_exe": build_exe_options},
-    executables=[Executable("app.py", base=None)],
+    executables=[Executable("app.py", base=base)],
 )
